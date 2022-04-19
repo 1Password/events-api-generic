33eaa17c409ad275b0a5945d77394a00980b9236
Jody Heavener <jody.heavener@agilebits.com>
2022-04-19 20:53:44 -0300

    Add examples across more languages, update readme

---
+++ eventsapi.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	var api_token = "Bearer " + os.Getenv("EVENTS_API_TOKEN")
+	var url = "https://events.1password.com"
+	if len(os.Getenv("EVENTS_API_URL")) > 0 {
+		url = os.Getenv("EVENTS_API_URL")
+	}
+
+	var start_time = time.Now().AddDate(0, 0, -24)
+
+	var payload = []byte(fmt.Sprintf(`{
+		"limit": 20,
+		"start_time": "%s"
+	}`, start_time.Format(time.RFC3339)))
+
+	request, error := http.NewRequest("POST", fmt.Sprintf("%s/api/v1/signinattempts", url), bytes.NewBuffer(payload))
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("Authorization", api_token)
+	client := &http.Client{}
+	response, error := client.Do(request)
+	if error != nil {
+		panic(error)
+	}
+	defer response.Body.Close()
+	body, _ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(body))
+}
---
+++ eventsapi.rb
@@ -0,0 +1,51 @@
+require 'date'
+require 'json'
+require 'net/http'
+require 'uri'
+
+# For more information, check out our support page
+# https://support.1password.com/events-reporting
+
+api_token = ENV['EVENTS_API_TOKEN']
+url = ENV['EVENTS_API_URL'] || 'https://events.1password.com'
+
+start_time = DateTime.now - 24
+
+headers = {
+  "Content-Type": "application/json",
+  "Authorization": "Bearer #{api_token}"
+}
+payload = {
+  "limit": 20,
+  "start_time": start_time.strftime("%FT%TZ")
+}
+
+# Alternatively, use the cursor returned from previous responses to get any new events
+# payload = { "cursor": cursor }
+
+uri = URI.parse(url+"/api/v1/signinattempts")
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true
+req = Net::HTTP::Post.new(uri.request_uri, headers)
+req.body = payload.to_json
+res = http.request(req)
+if (res.code == '200')
+  puts(JSON.parse(res.body))
+else
+  puts("Error getting sign in attempts: status code #{res.code}")
+end
+
+uri = URI.parse(url+"/api/v1/itemusages")
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true
+req = Net::HTTP::Post.new(uri.request_uri, headers)
+req.body = payload.to_json
+res = http.request(req)
+if (res.code == '200')
+  puts(JSON.parse(res.body))
+else
+  puts("Error getting item usages: status code #{res.code}")
+end
+
+# For more information on the response, check out our support page
+# https://support.1password.com/cs/events-api-reference/
