2bd051a3db52906e1c940f9bf6f347cd912aa6c7
Pro4TLZZ <github@pro4tlzz.com>
2022-05-17 17:19:32 +0100

    use request sessions

--- eventsapi.py
+++ eventsapi.py
@@ -10,10 +10,13 @@ url = "https://events.1password.com"
 
 start_time = datetime.datetime.now() - datetime.timedelta(hours=24)
 
+session = requests.Session()
+
 headers = {
   "Content-Type": "application/json",
   "Authorization": f"Bearer {api_token}"
 }
+session.headers.update(headers)
 payload = {
   "limit": 20,
   "start_time": start_time.astimezone().replace(microsecond=0).isoformat()
@@ -22,14 +25,14 @@ payload = {
 # Alternatively, use the cursor returned from previous responses to get any new events
 # payload = { "cursor": cursor }
 
-r = requests.post(f"{url}/api/v1/signinattempts", headers=headers, json=payload)
-if (r.status_code == requests.codes.ok):
+r = session.post(f"{url}/api/v1/signinattempts", headers=headers, json=payload)
+if (r.status_code == session.codes.ok):
   print(r.json())
 else:
   print("Error getting sign in attempts: status code", r.status_code)
 
-r = requests.post(f"{url}/api/v1/itemusages", headers=headers, json=payload)
-if (r.status_code == requests.codes.ok):
+r = session.post(f"{url}/api/v1/itemusages", headers=headers, json=payload)
+if (r.status_code == session.codes.ok):
   print(r.json())
 else:
   print("Error getting item usages: status code", r.status_code)
